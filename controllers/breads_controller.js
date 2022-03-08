060de9c9a8a30bb582e3cac07cb3b8042f0a9599
Saul Cortes <92839820+inscious@users.noreply.github.com>
2022-03-07 20:40:23 -0800

    sending index and showing data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -2,10 +2,20 @@ const express = require('express');
 const breads = express.Router();
 const Bread = require('../models/bread.js');
 
+// // INDEX
+// breads.get('/', (req, res) => {
+//   res.send(Bread);
+// });
+
 // INDEX
 breads.get('/', (req, res) => {
-  res.send(Bread);
-});
+    res.render('Index',
+      {
+        breads: Bread
+      }
+    )
+  // res.send(Bread)
+})
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
