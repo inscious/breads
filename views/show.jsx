15d5bc6718a792a97a5e4738f502e52ba8fe6103
Saul Cortes <92839820+inscious@users.noreply.github.com>
2022-03-12 13:48:07 -0800

    edit and update routes added

--- views/show.jsx
+++ views/show.jsx
@@ -4,21 +4,22 @@ const Default = require('./layouts/Default');
 function Show ({bread, index}) {
         return (
             <Default>
-            <h3>{bread.name}</h3>
-            <form action={`/breads/${index}?_method=DELETE`} method="POST">
-            <input type='submit' value="DELETE"/>
-            </form>
-            <p>
-                and it
-                {
-                bread.hasGluten
-                ? <span> does </span>
-                : <span> does NOT </span>
-                }
-                have gluten.
-            </p>
-            <img src={bread.image} alt={bread.name} />
-            <li><a href="/breads">Go home</a></li>
+                <h3>{bread.name}</h3>
+                <form action={`/breads/${index}?_method=DELETE`} method="POST">
+                    <input type='submit' value="DELETE"/>
+                </form>
+                <p>
+                    and it
+                    {
+                    bread.hasGluten
+                    ? <span> does </span>
+                    : <span> does NOT </span>
+                    }
+                    have gluten.
+                </p>
+                <img src={bread.image} alt={bread.name} />
+                <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+                <li> <a href="/breads">Go home</a> </li>
             </Default>
         )
     }
